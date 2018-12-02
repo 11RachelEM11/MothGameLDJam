065fd6b6b2537091bc1cb5df073ea55d9832c315
RachelEM <rachel.e.mailhot@gmail.com>
2018-12-02 16:30:20 -0500

    text stuff

--- Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -7,8 +7,18 @@ namespace UnityStandardAssets._2D
     [RequireComponent(typeof(PlatformerCharacter2D))]
     public class Platformer2DUserControl : MonoBehaviour
     {
+        public GameObject WinPlat;
+        public GameObject M1;
+        public GameObject R1;
+        public GameObject M2;
+        public GameObject M3;
+        public GameObject M4;
+        public GameObject M5;
+
         private PlatformerCharacter2D m_Character;
         private bool m_Jump;
+        private bool dupText = false;
+        private bool dupText2 = false;
 
 
         private void Awake()
@@ -42,6 +52,29 @@ namespace UnityStandardAssets._2D
             if (otherThing.tag == "NPC")
             {
                 print("I am colliding with" + otherThing);
+                if (dupText == false)
+                {
+                    Instantiate(M1);
+                    M1.transform.position = WinPlat.transform.position;
+                    dupText = true;
+                }
+            }
+            if (otherThing.tag == "NPC2")
+                if (dupText2 == false)
+                {
+                    Instantiate(M2);
+                    M2.transform.position = WinPlat.transform.position;
+                    dupText2 = true;
+                }
+            if (otherThing.tag == "NPC3")
+            {
+                print("I am colliding with" + otherThing);
+                if (dupText == false)
+                {
+                    Instantiate(M3);
+                    M3.transform.position = WinPlat.transform.position;
+                    dupText = true;
+                }
             }
         }
     }
